06258f85ba6e12dfef132c9ec1d53c26d0bdaba9
Japroz-Saini <sainijaproz@gmail.com>
2021-07-30 10:31:20 +0530

    add plausible.io analytics

--- dodge/src/pages/_document.tsx
+++ dodge/src/pages/_document.tsx
@@ -7,9 +7,20 @@ export default class Document extends NextDocument {
         return (
             <Html lang="en">
                 <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/> 
-<link rel="preconnect" href="https://fonts.gstatic.com" /> 
-<link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+                    <script
+                        defer
+                        data-domain="spam.japrozsaini.me"
+                        src="https://plausible.io/js/plausible.js"
+                    ></script>
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+                        rel="stylesheet"
+                    />
                     <link
                         rel="preconnect"
                         href="https://fonts.googleapis.com"
